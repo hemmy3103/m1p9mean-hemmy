43b601c1fe8ed1420784ff2ac52923f3c4b8cdf7
hemmy3103 <ramananjatohemmy@gmail.com>
2022-04-07 10:29:38 +0300

    server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/m1p9mean-hemmy"));
+app.use(express.static(__dirname + "/dist/m1p9mean-hemmy-front"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/m1p9mean-hemmy/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/m1p9mean-hemmy-front/index.html"));
 });
 app.listen(process.env.PORT || 8080);
